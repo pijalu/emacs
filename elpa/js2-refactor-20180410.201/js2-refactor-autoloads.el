227c590cc05b1a39738adfeb1d0e273cbf6fa923
Pierre Poissinger <Pierre.Poissinger@ordina.be>
2018-04-10 17:25:22 +0200

    Update

--- elpa/js2-refactor-20180410.201/js2-refactor-autoloads.el
+++ elpa/js2-refactor-20180410.201/js2-refactor-autoloads.el
@@ -3,8 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "js2-refactor" "js2-refactor.el" (23233 186
-;;;;;;  857751 81000))
+;;;### (autoloads nil "js2-refactor" "js2-refactor.el" (23244 55097
+;;;;;;  475842 633000))
 ;;; Generated autoloads from js2-refactor.el
 
 (autoload 'js2-refactor-mode "js2-refactor" "\
@@ -24,8 +24,8 @@ Add js2r keybindings using the modifier MODIFIER.
 
 ;;;***
 
-;;;### (autoloads nil "js2r-vars" "js2r-vars.el" (23233 186 848761
-;;;;;;  217000))
+;;;### (autoloads nil "js2r-vars" "js2r-vars.el" (23244 55097 465203
+;;;;;;  759000))
 ;;; Generated autoloads from js2r-vars.el
 
 (autoload 'js2r-extract-var "js2r-vars" "\
@@ -48,7 +48,7 @@ Add js2r keybindings using the modifier MODIFIER.
 ;;;### (autoloads nil nil ("js2-refactor-pkg.el" "js2r-conditionals.el"
 ;;;;;;  "js2r-conveniences.el" "js2r-formatting.el" "js2r-functions.el"
 ;;;;;;  "js2r-helpers.el" "js2r-iife.el" "js2r-paredit.el" "js2r-wrapping.el")
-;;;;;;  (23233 186 856326 323000))
+;;;;;;  (23244 55097 474092 484000))
 
 ;;;***
 
--- elpa/rsense-20100510.2105/rsense-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; rsense-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("rsense.el") (23243 46491 533502 142000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; rsense-autoloads.el ends here
