c95624c08408f4c3eb0e59fb6152caae5c41abd5
Pierre Poissinger <Pierre.Poissinger@ordina.be>
2018-04-23 21:38:12 +0200

    Updated deps

--- elpa/websocket-20180422.1716/websocket-autoloads.el
+++ elpa/websocket-20180422.1716/websocket-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil nil ("websocket.el") (23236 64199 998455 842000))
+;;;### (autoloads nil nil ("websocket.el") (23262 13826 683320 755000))
 
 ;;;***
 
